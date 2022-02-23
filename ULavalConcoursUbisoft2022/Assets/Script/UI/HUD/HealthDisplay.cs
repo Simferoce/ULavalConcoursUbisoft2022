801e509151e087c79515f2e0bcb90f70d6c88bdc
Crazytrain1 <maximeplourde0@gmail.com>
2022-02-23 16:04:25 -0500

    healthBar debut
    pas fonctionnel, ma fix ca a soir après ma job

--- ULavalConcoursUbisoft2022/Assets/Script/UI/HUD/HealthDisplay.cs
+++ ULavalConcoursUbisoft2022/Assets/Script/UI/HUD/HealthDisplay.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
-    
+    private Player player = null;
+    public  Health health = null;
+    public float maxHealth;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindObjectOfType<Player>();
+        health = player.GetComponentInChildren<Health>();
     }
-
+   
     // Update is called once per frame
-    void Update()
+    public void HealthChange(float health)
     {
+        slider.value = health;
         
     }
+    
 }
