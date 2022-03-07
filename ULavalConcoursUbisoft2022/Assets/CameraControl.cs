5c5c23f258cb5446beab72f8b910a1092f47fb76
Simon <le6monstre@hotmail.com>
2022-03-07 00:31:57 -0500

    Add safe buffer to camera so it wont alway follow direction player is looking at

--- ULavalConcoursUbisoft2022/Assets/CameraControl.cs
+++ ULavalConcoursUbisoft2022/Assets/CameraControl.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] private Transform _followOrigin = null;
+    [SerializeField] private Transform _cameraTarget = null;
+    [SerializeField] private Transform _player = null;
     [SerializeField] private CinemachineVirtualCamera _virtualCamera = null;
     [SerializeField] private CinemachineVirtualCamera _virtualUICamera = null;
+    [SerializeField] private Transform _aim = null;
+    [SerializeField] private Camera _mainCamera = null;
+    [Tooltip("In screen space coordinate (0...1) (0...1)")]
+    [SerializeField] private float _borderRadius = 0.0f;
+
+    private Plane _plane = new Plane(Vector3.up, -1);
+    private bool _focus = false;
 
     public void Focus(Transform transform)
     {
@@ -16,14 +24,59 @@ public class CameraControl : MonoBehaviour
 
         _virtualUICamera.Follow = transform;
         _virtualUICamera.LookAt = transform;
+
+        _focus = true;
     }
 
     public void ResetToOrigin()
     {
-        _virtualCamera.Follow = _followOrigin;
-        _virtualCamera.LookAt = _followOrigin;
+        _virtualCamera.Follow = _cameraTarget;
+        _virtualCamera.LookAt = _cameraTarget;
 
-        _virtualUICamera.Follow = _followOrigin;
-        _virtualUICamera.LookAt = _followOrigin;
+        _virtualUICamera.Follow = _cameraTarget;
+        _virtualUICamera.LookAt = _cameraTarget;
+
+        _focus = false;
+    }
+
+    private void Update()
+    {
+        if(!_focus)
+        {
+            Vector3 viewportPosition = _mainCamera.ScreenToViewportPoint(Input.mousePosition);
+            if (Vector2.Distance(new Vector2(viewportPosition.x, viewportPosition.y), new Vector2(0.5f, 0.5f)) > _borderRadius)
+            {
+                _virtualCamera.Follow = _cameraTarget;
+                _virtualCamera.LookAt = _cameraTarget;
+
+                _virtualUICamera.Follow = _cameraTarget;
+                _virtualUICamera.LookAt = _cameraTarget;
+
+                Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+                float enter = 0.0f;
+                if (_plane.Raycast(ray, out enter))
+                {
+                    Vector3 hitPoint = ray.GetPoint(enter);
+
+                    _aim.position = hitPoint;
+                }
+            }
+            else
+            {
+                _virtualCamera.Follow = _player;
+                _virtualCamera.LookAt = _player;
+
+                _virtualUICamera.Follow = _player;
+                _virtualUICamera.LookAt = _player;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(_aim.position, 0.3f);
     }
 }
--- ULavalConcoursUbisoft2022/Assets/Script/Entity/Player/Player.cs
+++ ULavalConcoursUbisoft2022/Assets/Script/Entity/Player/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController _characterController = null;
     [SerializeField] private float _speed = 0.0f;
-    [SerializeField] private Aim _aim = null;
+    [SerializeField] private Transform _aim = null;
     [SerializeField] private Entity _entity = null;
 
     private Vector3 direction = Vector3.zero;
