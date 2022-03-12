f6620156526b0c8f7f6e753120085a3d4f57a1b8
Crazytrain1 <maximeplourde0@gmail.com>
2022-03-11 21:10:39 -0500

    functioning leaderboard

--- ULavalConcoursUbisoft2022/Assets/Script/UI/Death/LeaderboardController.cs
+++ ULavalConcoursUbisoft2022/Assets/Script/UI/Death/LeaderboardController.cs
@@ -8,11 +8,13 @@ public class LeaderboardController : MonoBehaviour
     public InputField MemberID, PlayerScore;
     public int ID;
     public GameObject LeaderboardUI;
+    int MaxScores = 7;
+    public Text[] Entries;
     private void Start()
     {
 
         
-        LootLockerSDKManager.StartGuestSession("Player", (response) =>
+        LootLockerSDKManager.StartSession("Player", (response) =>
         {
             if (response.success)
             {
@@ -23,6 +25,38 @@ public class LeaderboardController : MonoBehaviour
             {
                 Debug.Log("Failed");
             }
+        });
+
+    }
+
+
+    public void ShowScores()
+    {
+        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) =>
+        {
+            if (response.success)
+            {
+                LootLocker.Requests.LootLockerLeaderboardMember[] scores = response.items;
+
+                for(int i = 0; i< scores.Length; i++)
+                {
+                    Entries[i].text = (scores[i].rank + ".   " + scores[i].score);
+
+                }
+                if(scores.Length< MaxScores)
+                {
+                    for(int i = scores.Length; i< MaxScores; i++)
+                    {
+                        Entries[i].text = (i + 1).ToString() + ".   none";
+                    }
+                }
+                
+            }
+            else
+            {
+                Debug.Log("Failed");
+            }
+
         });
 
     }
