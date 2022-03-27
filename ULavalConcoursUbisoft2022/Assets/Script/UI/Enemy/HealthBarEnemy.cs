d1995df68b886d8d9707061a2f00b75c2734c8c7
Simon <le6monstre@hotmail.com>
2022-03-27 06:25:19 -0400

    Add combo bar

--- ULavalConcoursUbisoft2022/Assets/Script/UI/Enemy/HealthBarEnemy.cs
+++ ULavalConcoursUbisoft2022/Assets/Script/UI/Enemy/HealthBarEnemy.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class HealthBarEnemy : MonoBehaviour
 {
-    [SerializeField] private Image _healthbarSprite;
+    [SerializeField] private Image _healthbarSprite = null;
     [SerializeField] private Health _health = null;
+    [SerializeField] private Image _comboBar = null;
+    [SerializeField] private float _comboTime = 0.0f;
+    [SerializeField] private float _speed = 0.0f;
+
     private Camera _cam;
+    private float _lastTimeLostHealth = 0.0f;
 
-
+    private bool _drainningComboBar = false;
     private void Awake()
     {
         _health.OnDamage += UpdateHealth;
@@ -25,12 +30,37 @@ public class HealthBarEnemy : MonoBehaviour
     {
         transform.rotation = Quaternion.Euler(Vector3.Scale((_cam.transform.rotation).eulerAngles, new Vector3(1, 0, 0)));
 
-        //transform.rotation = Quaternion.Euler(Vector3.Scale(Quaternion.LookRotation(transform.position - _cam.transform.position).eulerAngles, new Vector3(1, 1, 0)));
+        if (Time.time < _lastTimeLostHealth + _comboTime)
+        {
+            if(_drainningComboBar)
+            {
+                _comboBar.fillAmount = _healthbarSprite.fillAmount;
+                _drainningComboBar = false;
+            }
+            _healthbarSprite.fillAmount = _health.HealthPoint / _health.MaxHealth;
+        }
+        else
+        {
+            if (_comboBar.fillAmount != _health.HealthPoint / _health.MaxHealth)
+            {
+                _drainningComboBar = true;
+                _comboBar.fillAmount -= _speed * Time.deltaTime;
+
+                if (_comboBar.fillAmount < _health.HealthPoint / _health.MaxHealth)
+                {
+                    _comboBar.fillAmount = _health.HealthPoint / _health.MaxHealth;
+                }
+            }
+            else
+            {
+                _drainningComboBar = false;
+            }
+        }
     }
 
     public void UpdateHealth(Health health, float damage)
     {
-        _healthbarSprite.fillAmount = health.HealthPoint / health.MaxHealth;
+        _lastTimeLostHealth = Time.time;
     }
 
     private void OnDestroy()
