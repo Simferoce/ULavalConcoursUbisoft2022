17dc7f669c71affe9d8a7752f67538cec5275783
Crazytrain1 <maximeplourde0@gmail.com>
2022-04-03 12:24:11 -0400

    test stress bar vfx

--- ULavalConcoursUbisoft2022/Assets/Script/UI/HUD/HealthDisplay.cs
+++ ULavalConcoursUbisoft2022/Assets/Script/UI/HUD/HealthDisplay.cs
@@ -8,19 +8,30 @@ public class HealthDisplay : MonoBehaviour
 {
     private Player player = null;
     public  Health health = null;
+    public GameObject barr;
     public float maxHealth;
     public Slider slider;
     public float currentHealth;
+    public Image barrimage;
+
+    private Color startColor = Color.white;
+    public Color endColor = Color.green;
+    [Range(0, 10)]
+    public float speed = 15;
+
+    
 
     void Start()
     {
         player = GameObject.FindObjectOfType<Player>();
         health = player.GetComponentInChildren<Health>();
+        barrimage = barr.GetComponent<Image>();
+        startColor = barrimage.color;
         currentHealth = health.MaxHealth;
         slider.value = 0.0f;
         maxHealth = health.MaxHealth;
         slider.maxValue = maxHealth;
-       
+        
     }
 
     public void Update()
@@ -28,5 +39,13 @@ public class HealthDisplay : MonoBehaviour
         
         currentHealth = health.HealthPoint;
         slider.value = maxHealth - currentHealth;
+        barrimage.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time * speed, 1));
+    }
+    public void healthbareffect()
+    {
+
+        Debug.Log("hit");
+        barrimage.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time * speed, 1));
+        
     }
 }
